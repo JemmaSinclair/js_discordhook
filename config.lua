ebc38f056465ae39a1fdc8c6177ef0d26e91c269
JemmaSinclair <61020381+JemmaSinclair@users.noreply.github.com>
2025-04-07 15:33:22 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,9 +4,9 @@ Config = {
     AvatarURL = "",
     EmbedColor = 3447003, -- Blue (use https://www.spycolor.com to convert HEX to DEC)
     Webhooks = {
-         ["production"] = "https://discord.com/api/webhooks/1350904823985213583/PTsPH_r-KGfa30Zub0wzH1vKtFvRoTz0rK-NgfJ621uKNXsT38s7eQaFGIa0ev08aFma"
-        ,["development"] = "https://discord.com/api/webhooks/1350905033327251558/3u9zd6rwZ413YB_HN9zo46hg7bhZ2zR_9Setig_k4RyEB325xFj6xowE3RTrKfqAIihW"
-        ,["fallback"] = "https://discord.com/api/webhooks/1350905033327251558/3u9zd6rwZ413YB_HN9zo46hg7bhZ2zR_9Setig_k4RyEB325xFj6xowE3RTrKfqAIihW"
+         ["production"] = ""
+        ,["development"] = ""
+        ,["fallback"] = ""
     },
     Debug = true,
 }
